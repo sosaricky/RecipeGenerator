5b9b4f3b3e0115780893af74daddbaf98f77aa2c
Ricky Sosa <tecraulsosa@gmail.com>
2024-11-04 09:50:53 -0300

    routes for edit and update actions,translation for update

--- config/routes.rb
+++ config/routes.rb
@@ -23,7 +23,7 @@ Rails.application.routes.draw do
   end
 
   resources :users
-  resources :preferences, only: %i[index create new]
+  resources :preferences, only: %i[index create new show edit update]
   resources :recipes, only: %i[index create new show]
 
   namespace :api do
