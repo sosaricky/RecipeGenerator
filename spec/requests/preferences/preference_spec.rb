0cc042da8594bd9890c41eccc2edee6be10f13f6
Ricky Sosa <tecraulsosa@gmail.com>
2024-11-04 14:23:48 -0300

    test for destroy action

--- spec/requests/preferences/preference_spec.rb
+++ spec/requests/preferences/preference_spec.rb
@@ -64,13 +64,46 @@ describe 'Preferences' do
     context 'when fails' do
       let(:id) { preference.id + 1 }
 
-      it 'do\'nt update the preference' do
+      it 'doesn\'t update the preference' do
         subject
         expect(preference.reload.name).not_to eq('name_updated')
       end
 
       it 'have http status 404' do
         expect(subject).to eq(404)
+      end
+    end
+  end
+
+  describe 'DELETE destroy' do
+    subject { delete preference_path(id) }
+
+    let(:id) { preference.id }
+    let!(:preference) { create(:preference) }
+
+    context 'when success' do
+      it 'destroy the preference' do
+        expect { subject }.to change(Preference, :count).by(-1)
+      end
+
+      it 'have http status 302' do
+        expect(subject).to eq(302)
+      end
+
+      it 'redirect to index' do
+        expect(subject).to redirect_to(preferences_path)
+      end
+
+      context 'when fails' do
+        let(:id) { preference.id + 1 }
+
+        it 'doesn\'t destroy the preference' do
+          expect { subject }.not_to change(Preference, :count)
+        end
+
+        it 'have http status 404' do
+          expect(subject).to eq(404)
+        end
       end
     end
   end
