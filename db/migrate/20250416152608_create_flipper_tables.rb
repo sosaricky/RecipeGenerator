38c8c386fd1e8eeafbcaaecc9f86bab1f5152ab3
Ricky Sosa <tecraulsosa@gmail.com>
2025-04-16 12:30:01 -0300

    new migrates for flipper tables

--- db/migrate/20250416152608_create_flipper_tables.rb
+++ db/migrate/20250416152608_create_flipper_tables.rb
@@ -1,7 +1,5 @@
-# frozen_string_literal: true
-
-class CreateFlipperTables < ActiveRecord::Migration[7.0]
-  def self.up
+class CreateFlipperTables < ActiveRecord::Migration[7.1]
+  def up
     create_table :flipper_features do |t|
       t.string :key, null: false
       t.timestamps null: false
@@ -11,13 +9,13 @@ class CreateFlipperTables < ActiveRecord::Migration[7.0]
     create_table :flipper_gates do |t|
       t.string :feature_key, null: false
       t.string :key, null: false
-      t.string :value
+      t.text :value
       t.timestamps null: false
     end
-    add_index :flipper_gates, %i[feature_key key value], unique: true
+    add_index :flipper_gates, [:feature_key, :key, :value], unique: true, length: { value: 255 }
   end
 
-  def self.down
+  def down
     drop_table :flipper_gates
     drop_table :flipper_features
   end
