0f5b2f749791cb81aad13d9193b28296fe1853a0
Ricky Sosa <tecraulsosa@gmail.com>
2025-04-16 07:29:22 -0300

    add adapter for deepseek api

---
+++ app/services/adapters/deep_seek_adapter.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+class DeepSeekAdapter
+  def message_to_chat_api(request_messages)
+    headers = {
+      'Authorization' => "Bearer #{ENV.fetch('DEEPSEEK_API_KEY')}",
+      'Content-Type' => 'application/json'
+    }
+    body = {
+      model: ENV.fetch('DEEPSEEK_MODEL'),
+      messages: request_messages,
+      temperature: ENV.fetch('DEEPSEEK_TEMPERATURE').to_f
+    }.to_json
+
+    HTTParty.post(
+      'https://api.deepseek.com/chat/completions',
+      headers:,
+      body:
+    ).body
+  end
+end
